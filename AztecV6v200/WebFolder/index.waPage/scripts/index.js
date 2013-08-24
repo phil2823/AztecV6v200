9780f9dfd1a962707805340558ca25cadfb7bf94
phil2823 <philip2823@gmail.com>
2013-08-24 18:16:38 -0400

    Update #4
    working component loading

---
+++ AztecV6v200/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,31 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+	var menuStudents = {};	// @menuItem
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		// Add your code here
+		var checkLogin = WAF.directory.currentUser();
+		
+				
+	};// @lock
+
+	menuStudents.click = function menuStudents_click (event)// @startlock
+	{// @endlock
+		// Student Menu
+		alert('student menu');
+		$$('main').loadComponent('/views/students.waComponent');
+	
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+	WAF.addListener("menuStudents", "click", menuStudents.click, "WAF");
+// @endregion
+};// @endlock
