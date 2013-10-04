a44531ff99f1c36daaef8a687b1138659b63c57a
phil2823 <philip2823@gmail.com>
2013-10-04 15:55:00 -0400

    Update #13
    trying to get subviews working

--- AztecV6Student/WebFolder/views/viewsubClasses/announcements2.waComponent/announcements2.js
+++ AztecV6Student/WebFolder/views/viewsubClasses/announcements2.waComponent/announcements2.js
@@ -7,29 +7,17 @@ function constructor (id) {
 
 	// @region beginComponentDeclaration// @startlock
 	var $comp = this;
-	this.name = 'classes2';
+	this.name = 'announcements2';
 	// @endregion// @endlock
 
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
-	var clAnnouncementButton = {};	// @button
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
-	clAnnouncementButton.click = function clAnnouncementButton_click (event)// @startlock
-	{// @endlock
-		// Add your code here
-		//alert('should have loaded');
-	
-		$$('clcontainer2').loadComponent('/viewsubClasses/announcements2.waComponent');
-		
-	};// @lock
-	
-
 	// @region eventManager// @startlock
-	WAF.addListener(this.id + "_clAnnouncementButton", "click", clAnnouncementButton.click, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
