561f28bb9b05ac7385f1f4e1270b6d3798e712f7
phil2823 <philip2823@gmail.com>
2013-10-07 14:20:49 -0400

    Update #14
    have the containers in containers working but main menu is weird and trying to get main menu buttons colors

--- AztecV6Student/WebFolder/index.waPage/scripts/index.js
+++ AztecV6Student/WebFolder/index.waPage/scripts/index.js
@@ -2,15 +2,36 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var menuStudent = {};	// @menuItem
+	var menuSchool = {};	// @menuItem
 	var menuClasses = {};	// @menuItem
 	var documentEvent = {};	// @document
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	menuStudent.click = function menuStudent_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		alert('you hit my account');
+		$$('studentMain').loadComponent('/views/myAccount.waComponent');
+		cleanMainMenuItems();
+	};// @lock
+
+	menuSchool.click = function menuSchool_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		alert('hit school');
+		$$('studentMain').loadComponent('/views/school.waComponent');
+		cleanMainMenuItems();
+	};// @lock
+
 	menuClasses.click = function menuClasses_click (event)// @startlock
 	{// @endlock
+		alert('hit classes');
 		$$('studentMain').loadComponent('/views/classes2.waComponent');
+		cleanMainMenuItems();
+		
 	};// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
@@ -18,8 +39,13 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	
 	};// @lock
 		
-
+cleanMainMenuItems(){
+	// clean up all the items
+	};
+	
 // @region eventManager// @startlock
+	WAF.addListener("menuStudent", "click", menuStudent.click, "WAF");
+	WAF.addListener("menuSchool", "click", menuSchool.click, "WAF");
 	WAF.addListener("menuClasses", "click", menuClasses.click, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 // @endregion
--- AztecV6Student/WebFolder/views/classes2.waComponent/classes2.js
+++ AztecV6Student/WebFolder/views/classes2.waComponent/classes2.js
@@ -13,22 +13,99 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var clChatButton = {};	// @button
+	var clMessagesButton = {};	// @button
+	var clGroupsButton = {};	// @button
+	var clProgressButton = {};	// @button
+	var clInstructorButton = {};	// @button
+	var clLessonButton = {};	// @button
 	var clAnnouncementButton = {};	// @button
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+
+	clChatButton.click = function clChatButton_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		$$('studentMain_clcontainer').loadComponent('views/viewsubClasses/myChat.waComponent');
+		cleanbuttons();
+		$$('studentMain_clChatButton').setTextColor("blue");
+	};// @lock
+
+	clMessagesButton.click = function clMessagesButton_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		$$('studentMain_clcontainer').loadComponent('views/viewsubClasses/myMessages.waComponent');
+		cleanbuttons();
+		$$('studentMain_clMessagesButton').setTextColor("blue");
+	};// @lock
+
+	clGroupsButton.click = function clGroupsButton_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		$$('studentMain_clcontainer').loadComponent('views/viewsubClasses/myGroups.waComponent');
+		cleanbuttons();
+		$$('studentMain_clGroupsButton').setTextColor("blue");
+	};// @lock
+
+	clProgressButton.click = function clProgressButton_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		$$('studentMain_clcontainer').loadComponent('views/viewsubClasses/myProgress.waComponent');
+		cleanbuttons();
+		$$('studentMain_clProgressButton').setTextColor("blue");
+	};// @lock
+
+	clInstructorButton.click = function clInstructorButton_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		//alert("you hit instructor button");
+		//alert(getHtmlId('clcontainer'));
+		//WAF.loadComponent({id:'studentMain_clcontainer', path:'/views/viewsubClasses/instructors2.waComponent'});
+		$$('studentMain_clcontainer').loadComponent('views/viewsubClasses/instructors2.waComponent');
+		cleanbuttons();
+		$$('studentMain_clInstructorButton').setTextColor("blue");
+	};// @lock
+
+	clLessonButton.click = function clLessonButton_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		cleanbuttons();
+		$$('studentMain_clcontainer').loadComponent('/views/viewsubClasses/learningPlan.waComponent');
+		$$('studentMain_clLessonButton').setTextColor("blue");
+	};// @lock
+
 	clAnnouncementButton.click = function clAnnouncementButton_click (event)// @startlock
 	{// @endlock
 		// Add your code here
 		//alert('should have loaded');
-	
-		$$('clcontainer2').loadComponent('/viewsubClasses/announcements2.waComponent');
+		cleanbuttons();
+		$$('studentMain_clcontainer').loadComponent('/views/viewsubClasses/announcements2.waComponent');
+		$$('studentMain_clAnnouncementButton').setTextColor("blue");
 		
 	};// @lock
 	
-
+function cleanbuttons() {
+	// clear all the buttonss of colors restore them to original state
+	// I love building code automatically with emacs
+	$$('studentMain_clInstructorButton').setTextColor("#737373");
+	$$('studentMain_clLessonButton').setTextColor("#737373");
+	$$('studentMain_clAnnouncementButton').setTextColor("#737373");
+	$$('studentMain_clChatButton').setTextColor("#737373");
+	$$('studentMain_clMessagesButton').setTextColor("#737373");
+	$$('studentMain_clGroupsButton').setTextColor("#737373");
+	$$('studentMain_clProgressButton').setTextColor("#737373");
+	
+	};
+	
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_clChatButton", "click", clChatButton.click, "WAF");
+	WAF.addListener(this.id + "_clMessagesButton", "click", clMessagesButton.click, "WAF");
+	WAF.addListener(this.id + "_clGroupsButton", "click", clGroupsButton.click, "WAF");
+	WAF.addListener(this.id + "_clProgressButton", "click", clProgressButton.click, "WAF");
+	WAF.addListener(this.id + "_clInstructorButton", "click", clInstructorButton.click, "WAF");
+	WAF.addListener(this.id + "_clLessonButton", "click", clLessonButton.click, "WAF");
 	WAF.addListener(this.id + "_clAnnouncementButton", "click", clAnnouncementButton.click, "WAF");
 	// @endregion// @endlock
 
